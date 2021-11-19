a1b7e1715b0665f7e114a37ff5b9410665959a38
Maxim Dejnew <red41tiger@mail.ru>
2021-11-19 20:10:25 +0700

    Project outlook is under construction 12: implemented font-awesome icons components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { AppComponent } from './app.component';
 import { LinkComponent } from './components/link/link.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -19,7 +19,8 @@ import { LogotypeComponent } from './components/logotype/logotype.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub, faTelegram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare, faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  faGithub = faGithub;
+  faTelegram = faTelegram;
+  faEnvelopeSquare = faEnvelopeSquare;
+  faPhoneSquare = faPhoneSquare;
 
   constructor() { }
 
