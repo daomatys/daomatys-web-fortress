096d7b6813c4902c8fd1eff3f858bca34213758b
Maxim Dejnew <red41tiger@mail.ru>
2021-11-21 12:56:40 +0700

    Applied module changes to module script file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
-import { LinkComponent } from './components/link/link.component';
 import { HeaderComponent } from './components/header/header.component';
+import { Main__careersComponent } from './components/main/__careers/main__careers.component';
+import { Main__defaultComponent } from './components/main/__default/main__default.component';
+import { Main__portfolioComponent } from './components/main/__portfolio/main__portfolio.component';
+import { Main__resumeComponent } from './components/main/__resume/main__resume.component';
 import { FooterComponent } from './components/footer/footer.component';
+
+import { LinkComponent } from './components/link/link.component';
 import { LogotypeComponent } from './components/logotype/logotype.component';
 import { SocialIconComponent } from './components/social-icon/social-icon.component';
 
@@ -14,6 +19,10 @@ import { SocialIconComponent } from './components/social-icon/social-icon.compon
   declarations: [			
     AppComponent,
       HeaderComponent,
+      Main__careersComponent,
+      Main__defaultComponent,
+      Main__portfolioComponent,
+      Main__resumeComponent,
       FooterComponent,
       LinkComponent,
       LogotypeComponent,
