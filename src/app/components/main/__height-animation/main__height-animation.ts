17ff32b6acce8a05542a85db0337273d48384fbd
Maxim Dejnew <red41tiger@mail.ru>
2021-11-22 23:27:06 +0700

    Adjusted animation parameters 2

--- src/app/components/main/__height-animation/main__height-animation.ts
+++ src/app/components/main/__height-animation/main__height-animation.ts
@@ -2,68 +2,25 @@ import { trigger, state, query, group, style, transition, animate, animateChild 
 
 export const mainHeightAnimation:any =
   trigger('routeAnimations', [
-    transition('DefaultPage <=> ResumePage', [
+    transition('* <=> *', [
 
-      style({ position: 'relative' }),
-
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
+      style({
+        position: "relative",
+        left: 0
+      }),
 
       query(':enter', [
-        style({ left: '-100%' })
+        style({ opacity: '0%' })
       ]),
-
-      query(':leave', animateChild()),
 
       group([
         query(':leave', [
-          animate('300ms ease-out', style({ left: '100%' }))
+          animate('300ms ease-out', style({ opacity: '0%' }))
         ]),
 
         query(':enter', [
-          animate('300ms ease-out', style({ left: '0%' }))
+          animate('300ms ease-out', style({ opacity: '100%' }))
         ])
       ]),
-      
-      query(':enter', animateChild()),
-    ]),
-
-
-    transition('* <=> CareersPage', [
-
-      style({ position: 'relative' }),
-
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
-
-      query(':enter', [
-        style({ left: '-100%' })
-      ]),
-
-      query(':leave', animateChild()),
-
-      group([
-        query(':leave', [
-          animate('200ms ease-out', style({ left: '100%' }))
-        ]),
-
-        query(':enter', [
-          animate('300ms ease-out', style({ left: '0%' }))
-        ])
-      ]),
-
-      query(':enter', animateChild()),
     ])
   ]);
