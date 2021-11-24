6035eaebaecbb5aa5856d9ac441d9ebbcfd65a2f
Maxim Dejnew <red41tiger@mail.ru>
2021-11-24 23:56:54 +0700

    Added new blocks container

---
+++ src/app/components/main/__resume/main__resume.blocks.ts
@@ -0,0 +1,48 @@
+export interface ResumeBlocksInterface {
+  title: string;
+  texts: string[];
+};
+
+
+export const resumeBlocksArray = [
+  {
+    title: "whoami",
+    texts: [
+      "Matyskin Maxim / 26yo",
+      "junior front-end developer",
+      "unemployed "
+    ]
+  },
+  {
+    title: "languages",
+    texts: [
+      "russian — native",
+      "english — upper-intermediate"
+    ]
+  },
+  {
+    title: "programming languages",
+    texts: [
+      "javascript",
+      "typescript"
+    ]
+  },
+  {
+    title: "domain-specific languages",
+    texts: [
+      "angular",
+      "sqilte3"
+    ]
+  },
+  {
+    title: "hard skills",
+    texts: [
+      "html / pug",
+      "css / scss",
+      "readymag / figma",
+      "adobe ps",
+      "webpack",
+      "git cli"
+    ]
+  }
+]
