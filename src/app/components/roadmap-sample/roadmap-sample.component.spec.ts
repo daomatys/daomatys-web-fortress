0dab14aadafa3aa1d2df0b50595b6acb48d35a27
Maxim Dejnew <red41tiger@mail.ru>
2021-11-25 01:07:43 +0700

    Added new component

---
+++ src/app/components/roadmap-sample/roadmap-sample.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { RoadmapSampleComponent } from './roadmap-sample.component';
+
+describe('RoadmapSampleComponent', () => {
+  let component: RoadmapSampleComponent;
+  let fixture: ComponentFixture<RoadmapSampleComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RoadmapSampleComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RoadmapSampleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/roadmap-sample/roadmap-sample.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roadmap-sample',
+  templateUrl: './roadmap-sample.component.html',
+  styleUrls: ['./roadmap-sample.component.scss']
+})
+export class RoadmapSampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
