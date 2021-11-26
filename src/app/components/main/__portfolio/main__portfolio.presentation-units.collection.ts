abd011d070b0be9bce39e25ed8a9eb82268557c3
Maxim Dejnew <red41tiger@mail.ru>
2021-11-26 15:16:46 +0700

    Updated portfolio presentation-units collection

--- src/app/components/main/__portfolio/main__portfolio.presentation-units.collection.ts
+++ src/app/components/main/__portfolio/main__portfolio.presentation-units.collection.ts
@@ -1,22 +1,42 @@
+import {
+  faJsSquare,
+  faHtml5,
+  faCss3,
+  faSass,
+  faAngular
+} from '@fortawesome/free-brands-svg-icons';
+
 export default [
   {
     src:  "",
     text: "",
+    icons: [
+      ""
+    ],
     href: "https://github.com/daomatys/metalamp-fedsec-layouts"
   },
   {
     src:  "",
     text: "",
+    icons: [
+      ""
+    ],
     href: "https://github.com/daomatys/blackjack-wut"
   },
   {
     src:  "",
     text: "",
+    icons: [
+      ""
+    ],
     href: "https://github.com/daomatys/jsbasic-20210520_overmaxcoast"
   },
   {
     src:  "",
     text: "",
+    icons: [
+      ""
+    ],
     href: "https://github.com/daomatys/voenkom-tg-bot"
-  },
+  }
 ]
--- src/app/components/presentation-unit/__interface/presentation-unit__interface.ts
+++ src/app/components/presentation-unit/__interface/presentation-unit__interface.ts
@@ -1,5 +1,8 @@
+import { IconDefinition } from "@fortawesome/fontawesome-common-types";
+
 export interface PresentationUnitInterface {
   src: string;
   text: string;
   href: string;
+  icons: IconDefinition[];
 }
