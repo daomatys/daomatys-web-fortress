026b568c0eb6f39369da060998e85d0379dd414a
Maxim Dejnew <red41tiger@mail.ru>
2021-12-01 14:38:02 +0700

    Minor code optimization

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -13,38 +13,13 @@ import { mainAnimation } from './main.animation';
 
 export class MainComponent implements OnChanges, OnInit {
 
-  startHeight: number;
-  endHeight: number;
-  heights: {
-    start: number,
-    end: number
-  };
-
   constructor( private elem: ElementRef ) {}
 
-  ngOnInit() {
-    this.startHeight = this.elem.nativeElement.clientHeight;
-    this.endHeight = this.elem.nativeElement.clientHeight;
-    this.heights = {
-      start: this.startHeight,
-      end: this.endHeight
-    };
-  }
+  ngOnInit() {}
 
-  ngOnChanges() {
-    this.endHeight = this.elem.nativeElement.clientHeight;
-    console.log( this.endHeight, this.startHeight )
-
-
-
-    this.startHeight = this.endHeight;
-  }
+  ngOnChanges() {}
   
   prepareRoute( outlet: RouterOutlet ) {
-    this.heights = {
-      start: this.startHeight,
-      end: this.endHeight
-    };
     return outlet?.activatedRouteData?.['animation'];
   }
 }
