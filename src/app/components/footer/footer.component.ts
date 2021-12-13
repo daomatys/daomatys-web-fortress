8f7e0332e946a590233c2b509133e63480387ee0
Maxim Dejnew <red41tiger@mail.ru>
2021-12-13 23:51:03 +0700

    Implemented new "red eyes" recursive applicating animation

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 import { SocialIconInterface } from '../social-icon/social-icon.interface';
 import socialIconsItems from './footer.social-icons-collection';
@@ -9,11 +9,38 @@ import socialIconsItems from './footer.social-icons-collection';
   styleUrls: ['./footer.component.scss']
 })
 
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
   public socialIcons:SocialIconInterface[] = socialIconsItems;
   public changeableWord:string = 'call';
 
   constructor() {}
+
+  ngAfterViewInit():void {
+    const wrap = ( document.querySelector('#red-eyes') as HTMLObjectElement );
+
+    wrap?.addEventListener('load', () => {
+      const coverage = ( wrap.contentDocument?.querySelector('#red-eyes__coverage') as Element );
+
+      this.recursiveWinkAnimation( coverage );
+    });
+  }
+
+  recursiveWinkAnimation( coverage:Element ) {
+    const applyAnimation = function applyAnimationForReal():void {
+      const randomTime = Math.floor( Math.random() * Math.floor( 10000 ) );
+
+      const animation = coverage?.animate({
+        transform: [ "translateY(3px)", "translateY(0px)" ]
+      }, {
+        duration: 500,
+        easing: "ease",
+      });
+
+      animation.onfinish = () => setTimeout( applyAnimation, randomTime )
+    }
+
+    applyAnimation();
+  }
 
   onMouseEnterEvent( marker:string ):void {
     this.changeableWord = marker;
