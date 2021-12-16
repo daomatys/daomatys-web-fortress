74b55712e157318a951bf449b576d8a752d89367
Maxim Dejnew <red41tiger@mail.ru>
2021-12-16 18:20:04 +0700

    Trying to debug all that observable stuff

--- src/app/services/title-page-content-initializer/title-page-content-initializer.service.ts
+++ src/app/services/title-page-content-initializer/title-page-content-initializer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +7,22 @@ import { BehaviorSubject } from 'rxjs';
 
 export class TitlePageContentInitializerService {
   private caseMessageSource = new BehaviorSubject( false );
+
   public caseCurrentPageIsTitle = this.caseMessageSource.asObservable();
 
   constructor() {}
 
   setStateFalse():void {
-    this.caseMessageSource.next( false )
+    this.caseMessageSource.next( false );
+    this.debug( this.caseCurrentPageIsTitle );
   }
 
   setStateTrue():void {
-    this.caseMessageSource.next( true )
+    this.caseMessageSource.next( true );
+    this.debug( this.caseCurrentPageIsTitle );
+  }
+
+  debug( data:Observable<boolean> ):void {
+    data.subscribe( message=>console.log(message) );
   }
 }
